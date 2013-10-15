95cecaa66f86197a59d0a6e7ba97f39827213a06
ahastudio <ahastudio@gmail.com>
2013-10-16 02:27:02 +0900

    이미지 목록 갯수를 바꾸기 쉽게 함

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,31 +1,7 @@
 'use strict';
 
 function initImages() {
-  var images = [
-    {
-      title: 'Google Logo',
-      url: "http://allaboutetp.files.wordpress.com/2012/07/google-logo2.jpeg"
-    },
-    {
-      title: 'Google Letter',
-      url: "http://cfs7.blog.daum.net/original/27/blog/2007/11/16/13/57/473d2334a5973"
-    },
-    {
-      title: 'Google Screenshot',
-      url: "http://hooney.net/wp/wp-content/uploads/2007/05/google-kr.png"
-    },
-    {
-      title: 'Google Logo',
-      url: "http://allaboutetp.files.wordpress.com/2012/07/google-logo2.jpeg"
-    },
-    {
-      title: 'Google Letter',
-      url: "http://cfs7.blog.daum.net/original/27/blog/2007/11/16/13/57/473d2334a5973"
-    },
-    {
-      title: 'Google Screenshot',
-      url: "http://hooney.net/wp/wp-content/uploads/2007/05/google-kr.png"
-    },
+  var imageDatas = [
     {
       title: 'Google Logo',
       url: "http://allaboutetp.files.wordpress.com/2012/07/google-logo2.jpeg"
@@ -40,11 +16,14 @@ function initImages() {
     },
   ];
 
-  _.each(images, function(image, index) {
-    image.id = index;
+  return _.map(_.range(21), function(index) {
+    var imageData = imageDatas[index % imageDatas.length];
+    return {
+      id: index,
+      title: imageData.title,
+      url: imageData.url
+    };
   });
-
-  return images;
 }
 
 angular.module('imageBookmarkApp')

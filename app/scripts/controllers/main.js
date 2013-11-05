d6dd833bc7687aebe25b0d7e8c41c250dcab5363
ahastudio <ahastudio@gmail.com>
2013-11-06 07:20:27 +0900

    이미지 보여주기

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,11 +26,28 @@ function initImages() {
   });
 }
 
+// http://developer.chrome.com/apps/app_codelab8_webresources.html
+var loadImage = function(uri, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'blob';
+  xhr.onload = function() {
+    callback(window.webkitURL.createObjectURL(xhr.response), uri);
+  }
+  xhr.open('GET', uri, true);
+  xhr.send();
+}
+
 angular.module('imageBookmarkApp')
   .controller('MainCtrl', function ($scope) {
     $scope.images = [];
     chrome.storage.sync.get(['images'], function(items) {
-      $scope.images = items.images || [];
+      var images = items.images || [];
+      _.each(images, function(image) {
+        loadImage(image.url, function(blob_uri, requested_uri) {
+          image.uri = blob_uri;
+        });
+      });
+      $scope.images = images;
     });
     var image_id = $scope.images.length;
 
@@ -38,11 +55,19 @@ angular.module('imageBookmarkApp')
       var url = 'http://hooney.net/wp/wp-content/uploads/2007/05/google-kr.png';
       var title = 'Google Screenshot';
       image_id++;
-      $scope.images.push({id:image_id, title:title, url:url});
+      var image = {
+        id: image_id,
+        title: title,
+        url: url
+      };
+      $scope.images.push(image);
       chrome.storage.sync.set({
         images: $scope.images
       }, function() {
         // do nothing...
+      });
+      loadImage(image.url, function(blob_uri, requested_uri) {
+        image.uri = blob_uri;
       });
     };
 

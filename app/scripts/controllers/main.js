eb724ca8e1056ab9b32849ca5103e08ced380875
ahastudio <ahastudio@gmail.com>
2013-11-06 07:35:59 +0900

    안 쓰는 코드 삭제

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,42 +1,5 @@
 'use strict';
 
-function initImages() {
-  var imageDatas = [
-    {
-      title: 'Google Logo',
-      url: "http://allaboutetp.files.wordpress.com/2012/07/google-logo2.jpeg"
-    },
-    {
-      title: 'Google Letter',
-      url: "http://cfs7.blog.daum.net/original/27/blog/2007/11/16/13/57/473d2334a5973"
-    },
-    {
-      title: 'Google Screenshot',
-      url: "http://hooney.net/wp/wp-content/uploads/2007/05/google-kr.png"
-    },
-  ];
-
-  return _.map(_.range(4), function(index) {
-    var imageData = imageDatas[index % imageDatas.length];
-    return {
-      id: index,
-      title: imageData.title,
-      url: imageData.url
-    };
-  });
-}
-
-// http://developer.chrome.com/apps/app_codelab8_webresources.html
-var loadImage = function(uri, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'blob';
-  xhr.onload = function() {
-    callback(window.webkitURL.createObjectURL(xhr.response), uri);
-  }
-  xhr.open('GET', uri, true);
-  xhr.send();
-}
-
 if (chrome.storage == undefined) {
   chrome.storage = {
     sync: {
@@ -57,6 +20,17 @@ if (chrome.storage == undefined) {
       callback(uri, uri);
     }, 1);
   };
+}
+
+// http://developer.chrome.com/apps/app_codelab8_webresources.html
+var loadImage = function(uri, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'blob';
+  xhr.onload = function() {
+    callback(window.webkitURL.createObjectURL(xhr.response), uri);
+  }
+  xhr.open('GET', uri, true);
+  xhr.send();
 }
 
 angular.module('imageBookmarkApp')
